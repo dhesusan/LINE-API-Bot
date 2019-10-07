e40c0c8584875c8f468af4193a856709965843f0
dhesusan <dhesusan4649@gmail.com>
2019-10-08 01:19:26 +0900

    add to search vacant hotel

--- top_api/views.py
+++ top_api/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponseForbidden, HttpResponse
+from django.http import (
+    HttpResponseForbidden,
+    HttpResponse
+)
 from django.views.decorators.csrf import csrf_exempt
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import (
+    LocationMessage,
     MessageEvent,
     TextMessage,
     TextSendMessage,
+    TemplateSendMessage,
+    CarouselColumn,
+    CarouselTemplate,
+    URITemplateAction
 )
+from configparser import ConfigParser
 import os
+import sys
+import requests
+import datetime
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
@@ -32,3 +49,77 @@ def callback(request):
 def handle_text_message(event):
     line_bot_api.reply_message(event.reply_token,
                                TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location(event):
+    result = search_vacant_hotel(event)
+
+    try:
+        if 'error' in result:
+            if result['error'] == 'not_found':
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='空室があるホテルが存在しませんでした。')
+                )
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='現在使用出来ません。')
+                )
+        else:
+            hotels = result['hotels']
+            columns = [
+                CarouselColumn(
+                    thumbnail_image_url=hotel[0]['hotelBasicInfo']['hotelimageUrl'],
+                    title=hotel[0]['hotelBasicInfo']['hotelName'][:40],
+                    text=hotel[0]['hotelBasicInfo']['hotelSpecial'][:60],
+                    actions=[
+                        URITemplateAction(
+                            label='詳細を見る',
+                            uri=hotel[0]['hotelBasicInfo']['hotelInformationUrl']
+                        )
+                    ]
+                )
+                for hotel in hotels
+            ]
+            messages = TemplateSendMessage(
+                alt_text='template',
+                template=CarouselTemplate(columns=columns),
+            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                messages
+            )
+            
+    except Exception as e:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='時間をおいて再度お試しください。')
+        )
+
+
+RAKUTEN_APPLICATION_ID = os.environ('RAKUTEN_APPLICATION_ID')
+VACANT_HOTEL_SEARCH_URL = os.environ('VACANT_HOTEL_SEARCH_URL')
+
+
+def search_vacant_hotel(event):
+    checkin_date = datetime.date.today()
+    checkout_date = checkin_date + datetime.timedelta(days=1)
+    
+    item_parameters = {
+        'applicationId': RAKUTEN_APPLICATION_ID,
+        'format': 'json',
+        'formatVersion': 2,
+        'latitude': event.message.latitude,
+        'longitude': event.message.longitude,
+        'datumType': 1,
+        'checkinDate': checkin_date,
+        'checkoutDate': checkout_date,
+        'searchRadius': 1,
+        'hits': 3,
+        'elements': 'hotelImageUrl,hotelName,hotelSpecial,hotelInformationUrl'
+    }
+    r = requests.get(VACANT_HOTEL_SEARCH_URL, params=item_parameters)
+    return r.json()
+
